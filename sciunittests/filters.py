f7e365bdb49e792599a85a0541e6d794edca5fa3
NeZanyat <rlatyshenko.dev@gmail.com>
2018-01-29 17:33:40 +0200

    Updated admin panel, some changes with filters required with frontend

--- sciunittests/filters.py
+++ sciunittests/filters.py
@@ -5,17 +5,43 @@ from sciunittests.models import Score, TestSuite, TestInstance
 class ScoreFilter(filters.FilterSet):
     timestamp_after = filters.IsoDateTimeFilter(name='timestamp',
             lookup_expr='gte')
+
     timestamp_before = filters.IsoDateTimeFilter(name='timestamp',
             lookup_expr='lte')
+
+    model_class = filters.CharFilter(
+            name='model_instance__model_class__class_name',
+            lookup_expr='startswith' )
+
+    test_class = filters.CharFilter(
+            name='test_instance__test_class__class_name',
+            lookup_expr='startswith' )
+
+    hostname = filters.CharFilter(
+            name='test_instance__hostname',
+            lookup_expr='startswith' )
+
+    score_name = filters.CharFilter(
+            name='test_instance__test_class__class_name',
+            lookup_expr='startswith' )
+
+    score_type = filters.CharFilter(
+            name='score_type',
+            lookup_expr='startswith' )
+
+    build_info = filters.CharFilter(
+            name='test_instance__build_info',
+            lookup_expr='startswith' )
 
     class Meta:
         model = Score
         fields = ['owner',
                 'model_instance',
-                'model_instance__model_class__class_name',
-                'test_instance__test_class__class_name',
-                'test_instance__hostname',
-                'test_instance__build_info',
+                'model_class',
+                'test_class',
+                'score_name',
+                'hostname',
+                'build_info',
                 'score_type',
                 'timestamp_after',
                 'timestamp_before']
