4bae461b3aa69696179410902ee89d2bbb9ba0fc
NeZanyat <rlatyshenko.dev@gmail.com>
2018-02-20 15:28:30 +0200

    New Admin page

--- sciunitmodels/admin.py
+++ sciunitmodels/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from sciunitmodels.models import ModelClass
+from sciunitmodels.models import ModelClass, ModelInstance
 
 # Register your models here.
 
 admin.site.register(ModelClass)
+admin.site.register(ModelInstance)
