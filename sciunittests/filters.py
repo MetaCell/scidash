30320e333ac0611697695eb55ac0c2743cd1d706
NeZanyat <rlatyshenko.dev@gmail.com>
2018-05-31 17:54:21 +0300

    Updated Filter for models

--- sciunittests/filters.py
+++ sciunittests/filters.py
@@ -1,4 +1,4 @@
-from django.db.models import Count, Subquery
+from django.db.models import Count, Subquery, Q
 
 from django_filters import rest_framework as filters
 from sciunittests.models import ScoreInstance, TestSuite, TestInstance
@@ -14,35 +14,39 @@ class ScoreFilter(filters.FilterSet):
     timestamp_before = filters.IsoDateTimeFilter(name='timestamp',
             lookup_expr='lte')
 
-    model_class = filters.CharFilter(
-            name='model_instance__model_class__class_name',
-            lookup_expr='contains' )
+    model_name = filters.CharFilter(method='model_class_name_filter')
 
     test_class = filters.CharFilter(
             name='test_instance__test_class__class_name',
-            lookup_expr='contains' )
+            lookup_expr='contains')
 
     hostname = filters.CharFilter(
             name='test_instance__hostname',
-            lookup_expr='contains' )
+            lookup_expr='contains')
 
     score_name = filters.CharFilter(
             name='test_instance__test_class__class_name',
-            lookup_expr='contains' )
+            lookup_expr='contains')
 
     score_type = filters.CharFilter(
             name='score_type',
-            lookup_expr='contains' )
+            lookup_expr='contains')
 
     build_info = filters.CharFilter(
             name='test_instance__build_info',
-            lookup_expr='contains' )
+            lookup_expr='contains')
 
     with_suites = filters.BooleanFilter(method='with_suites_filter')
 
     suite_name = filters.CharFilter(method='suite_name_filter')
 
     suite_hash = filters.CharFilter(method='suite_hash_filter')
+
+    def model_class_name_filter(self, queryset, name, value):
+        return ScoreInstance.objects.filter(
+                Q(model_instance__model_class__class_name__contains=value) |
+                Q(model_instance__name__contains=value)
+                )
 
     def with_suites_filter(self, queryset, name, value):
         tests = TestInstance.objects.prefetch_related('test_suites')
@@ -55,9 +59,8 @@ class ScoreFilter(filters.FilterSet):
         suites = TestSuite.objects.filter(name__contains=value)
 
         kwargs = {
-                'test_instance__test_suites__in':Subquery(suites.values('pk'))
+                'test_instance__test_suites__in': Subquery(suites.values('pk'))
                 }
-
 
         return queryset.filter(**kwargs)
 
@@ -65,7 +68,7 @@ class ScoreFilter(filters.FilterSet):
         suites = TestSuite.objects.filter(hash=value)
 
         kwargs = {
-                'test_instance__test_suites__in':Subquery(suites.values('pk'))
+                'test_instance__test_suites__in': Subquery(suites.values('pk'))
                 }
 
         return queryset.filter(**kwargs)
@@ -74,7 +77,7 @@ class ScoreFilter(filters.FilterSet):
         model = ScoreInstance
         fields = ['owner',
                 'model_instance',
-                'model_class',
+                'model_name',
                 'test_class',
                 'score_name',
                 'hostname',
