f5a07a72e2fc0d8324b2a4150ddf2929f98be32b
NeZanyat <NeZanyat@users.noreply.github.com>
2018-06-30 16:42:48 +0300

    Update db-backup.sh

--- db-backup.sh
+++ db-backup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 current_date=$(date +'%m-%d-%Y')
-
+db_name="scidash"
+db_user="scidash_admin"
 outfile="$current_date.backup"
 
-pg_dump -d $db_name > $outfile
+docker exec scidash_scidash-postgres_1 bash -lc 'pg_dump $db_name -U $db_user' > ./pg_dump.pgdata
