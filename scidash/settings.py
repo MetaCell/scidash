6ec134bfcf9d4498978c81ad8cb74c72483ccc1f
Giovanni Idili <giovanni.idili@gmail.com>
2018-07-31 17:06:16 +0100

    removed unneeded config bit

--- scidash/settings.py
+++ scidash/settings.py
@@ -165,18 +165,6 @@ STATICFILES_DIRS = [
 
 AUTH_USER_MODEL = 'general.ScidashUser'
 
-JWT_AUTH = {
-    # how long the original token is valid for
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=2),
-
-    # allow refreshing of tokens
-    'JWT_ALLOW_REFRESH': True,
-
-    # this is the maximum time AFTER the token was issued that
-    # it can be refreshed.  exprired tokens can't be refreshed.
-    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
-}
-
 REST_FRAMEWORK_CACHE = {
     'DEFAULT_CACHE_TIMEOUT': 86400, # Default is 1 day
 }
