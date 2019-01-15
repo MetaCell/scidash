c28230e51cff6ff671725f786bab33bc35ba59cf
NeZanyat <rlatyshenko.dev@gmail.com>
2019-01-15 16:20:07 +0200

    Updates

--- scidash/general/urls.py
+++ scidash/general/urls.py
@@ -3,5 +3,5 @@ from scidash.general.views import FileUploadView, GeppettoHandlerView
 
 urlpatterns = [
         url(r'^upload/(?P<filename>[^/]+)$', FileUploadView.as_view()),
-        url(r'^experiment-result/handle/$', GeppettoHandlerView.as_view() )
+        url(r'^experiment-result/handle$', GeppettoHandlerView.as_view())
         ]
