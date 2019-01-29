7b43285d1941e050b75eccd4ec3d6a3e96dcf800
NeZanyat <NeZanyat@users.noreply.github.com>
2019-01-29 17:42:44 +0200

    Update views.py

--- scidash/sciunitmodels/api/views.py
+++ scidash/sciunitmodels/api/views.py
@@ -19,7 +19,7 @@ class ModelClassViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = (permissions.AllowAny,)
 
 
-class ModelInstanceViewSet(viewsets.ReadOnlyModelViewSet):
+class ModelInstanceViewSet(viewsets.ModelViewSet):
     queryset = ModelInstance.objects.all()
     serializer_class = ModelInstanceSerializer
     permission_classes = (permissions.AllowAny,)
