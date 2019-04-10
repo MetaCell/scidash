ba04630421a406fdd013beca814b35459f3920ff
Roman <roman@Pro-15.local>
2019-04-10 22:51:51 +0300

    Score scheduling

--- scidash/general/views.py
+++ scidash/general/views.py
@@ -1,13 +1,19 @@
 import json
 import os
+import collections
+import re
 
 from django.views import View
+from django.conf import settings
 from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from scidash.sciunittests.serializers import ScoreInstanceSerializer
+from scidash.sciunittests.models import ScoreInstance
+from scidash.general import helpers as general_hlp
+from scidash.sciunitmodels import helpers as model_hlp
 
 
 class FileUploadView(APIView):
@@ -49,21 +55,108 @@ class FileUploadView(APIView):
 class GeppettoHandlerView(View):
 
     OUTPUT_MAPPING_FILE = 'outputMapping.dat'
+    RESULTS_FILE = 'results0.dat'
+
+    RESULTS_MAP = {
+        '^[\w\.\[\]]+\.v$': 'v',
+        '^time\(\w+\)$': 't'
+    }
+
+    def get_variable_from_header(self, header):
+        for key, value in self.RESULTS_MAP.items():
+            if re.match(key, header):
+                return value
+
+    def calculate_score(self, simulation_result, score_instance):
+        model_class = general_hlp.import_class(
+            score_instance.model_instance.model_class.import_path
+        )
+
+        model_url = score_instance.model_instance.url
+        model_name = os.path.basename(model_url)
+        model_path = os.path.join(
+            settings.DOWNLOADED_MODEL_DIR, model_name
+        )
+
+        model_hlp.download_and_save_model(model_path, model_url)
+
+        model_instance = model_class(
+            model_path, name=score_instance.model_instance.name
+        )
+
+        model_instance.get_backend().use_memory_cache = True
+        model_instance.get_backend().set_memory_cache(simulation_result)
+
+        test_class = general_hlp.import_class(
+            score_instance.test_instance.test_class.import_path
+        )
+
+        observation = score_instance.test_instance.observation
+        units = general_hlp.import_class(
+            score_instance.test_instance.test_class.units
+        )
+
+        for key in observation:
+            observation[key] = int(observation[key]) * units
+
+        test_instance = test_class(observation=observation)
+
+        // TODO: uncomment it and remove fake score setting after Rick's response
+        # score = test_instance.judge(model_instance)
+
+        score_instance.score = 0.1
+        score_instance.status = score_instance.CALCULATED
+        score_instance.sort_key = 0.1
+
+        score_instance.save()
 
     def post(self, request):
-        print(request.POST)
-
         body = json.loads(request.body)
         results = json.loads(body.get('experiment_results', ''))
+        score_pk = body.get('scoreID')
 
-        output_mapping = filter(
+        try:
+            score_instance = ScoreInstance.objects.get(pk=score_pk)
+        except ScoreInstance.DoesNotExists:
+            return Response({
+                'success': False,
+                'message': 'Score not found'
+            }, 404)
+
+        output_mapping = list(filter(
             lambda x: os.path.basename(x) == self.OUTPUT_MAPPING_FILE,
             results
-            )
+        )).pop().replace('file:', '')
 
-        output_mapping = output_mapping.pop()
+        results_file = list(filter(
+            lambda x: os.path.basename(x) == self.RESULTS_FILE,
+            results
+        )).pop().replace('file:', '')
+
+        simulation_result = None
 
         with open(output_mapping, 'r') as f:
-            
+            splat = f.read().split('\n')
 
-        return Response(request.POST)
+            simulation_result = collections.OrderedDict(
+                {
+                    self.get_variable_from_header(header): None
+                    for header in splat[1].split(' ')
+                }
+            )
+
+        with open(results_file, 'r') as f:
+            values = [[], []]
+
+            for line in f:
+                splat = line.split('	')
+
+                values[0].append(splat[0])
+                values[1].append(splat[1])
+
+        for i, (key, value) in enumerate(simulation_result.items()):
+            simulation_result[key] = values[i]
+
+        self.calculate_score(simulation_result, score_instance)
+
+        return Response(json.dumps(simulation_result))
