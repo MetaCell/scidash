e5d13ce23f82cee220a55317290182454b875a40
Roman <roman@Pro-15.local>
2019-04-16 15:38:20 +0300

    New run command

--- service/scripts/run.sh
+++ service/scripts/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 source ./venv/bin/activate;
-python manage.py migrate;
 cat ./service/scripts/create_superuser.py | ./manage.py shell;
-python manage.py runserver 0.0.0.0:8000;
+make run-staging
