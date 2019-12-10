ac7817a749c98927b16818a29c42e27bc1464def
Dario Del Piano <dario@metacell.us>
2019-12-10 12:59:50 +0000

    permission issue

--- service/scripts/db-create-psql.sh
+++ service/scripts/db-create-psql.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+cd /tmp
 git clone https://github.com/ddelpiano/scidash-artifacts
 cd scidash-artifacts/database
 gunzip `ls | head -n 1`
 pg_restore --clean -d scidash `ls *db | head -n 1`
+cd -
