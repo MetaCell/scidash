596c1fad4b643f4951b67567cbdca600ad5bbb0e
Dario Del Piano <dario@metacell.us>
2019-12-10 12:53:40 +0000

    typo sql instead of db file

--- service/scripts/db-create-psql.sh
+++ service/scripts/db-create-psql.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-git clone https://github.com/ddelpiano/scidash-artifacts
-cd scidash-artifacts/database
+cd /app/scidash-artifacts/database
 gunzip `ls | head -n 1`
-pg_restore --clean -d scidash `ls *sql | head -n 1`
+pg_restore --clean -d scidash `ls *db | head -n 1`
