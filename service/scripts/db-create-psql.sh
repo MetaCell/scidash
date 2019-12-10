ec01b5e830d801875bb7da549d32fd7661009731
Dario Del Piano <dario@metacell.us>
2019-12-10 12:58:02 +0000

    typo part 2

--- service/scripts/db-create-psql.sh
+++ service/scripts/db-create-psql.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-cd /app/scidash-artifacts/database
+git clone https://github.com/ddelpiano/scidash-artifacts
+cd scidash-artifacts/database
 gunzip `ls | head -n 1`
 pg_restore --clean -d scidash `ls *db | head -n 1`
