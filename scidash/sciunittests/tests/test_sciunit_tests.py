cc3e3bb393c4a6ac27ab4271fce308778ea2d3ca
Zoran Sinnema <zsinnema@gmail.com>
2020-01-11 14:49:00 -0500

    chore: revert breaking test

--- scidash/sciunittests/tests/test_sciunit_tests.py
+++ scidash/sciunittests/tests/test_sciunit_tests.py
@@ -482,4 +482,3 @@ class SciunitTestMatchingClassObjects(TestCase):
         parsed_response = response.json()
 
         self.assertEqual(len(parsed_response), 1)
-        self.assertEqual(1, 2) # test for travis integration test failing
