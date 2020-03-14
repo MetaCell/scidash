822f43c9c8d07119940803b9d71c96a2a6ff0f08
Zoran Sinnema <zsinnema@gmail.com>
2020-03-14 10:25:37 -0500

    saving wip

--- scidash/general/admin.py
+++ scidash/general/admin.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.contenttypes.models import ContentType
 
-from scidash.general.models import ScidashUser, Tag
+from adminsortable2.admin import SortableAdminMixin
+from scidash.general.models import ScidashUser, Tag, ContentItem
+
 
 # Register your models here.
+
+class ContentItemAdmin(SortableAdminMixin, admin.ModelAdmin):
+    list_display = ["name", "display_from", "display_to"]
+    date_hierarchy = "display_from"
+
 
 admin.site.register(ScidashUser, UserAdmin)
 admin.site.register(ContentType)
 admin.site.register(Tag)
+admin.site.register(ContentItem, ContentItemAdmin)
--- scidash/general/models.py
+++ scidash/general/models.py
@@ -3,6 +3,8 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
+from ckeditor.fields import RichTextField
+
 
 class ScidashUser(AbstractUser):
     class Meta:
@@ -21,3 +23,14 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ContentItem(models.Model):
+    content_order = models.PositiveIntegerField(default=0, blank=False, null=False)
+    name = models.CharField(max_length=100)
+    content = RichTextField()
+    display_from = models.DateTimeField(null=True, blank=True)
+    display_to = models.DateTimeField(null=True, blank=True)
+
+    class Meta(object):
+        ordering = ['content_order']
