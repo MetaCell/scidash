cca19f7d3058ce5452f5c5f2e86fae1ea5c0a0ba
Zoran Sinnema <zsinnema@gmail.com>
2021-04-21 16:46:18 +0200

    chore: added ddelpiano/sciunit@4.0.0 to requirements

--- service/docker/build_codefresh.sh
+++ service/docker/build_codefresh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -f Dockerfile-virgo -t metacell/scidash_virgo:latest
-docker build -f Dockerfile-postgres -t metacell/scidash_db:latest
-docker build -f Dockerfile-scidash -t metacell/scidash:latest
+docker build -f Dockerfile-virgo -t metacell/scidash_virgo:latest .
+docker build -f Dockerfile-postgres -t metacell/scidash_db:latest .
+docker build -f Dockerfile-scidash -t metacell/scidash:latest .
