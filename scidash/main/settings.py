5d276cdf9ffa118261421c664d9d015da29ff0ca
Zoran Sinnema <zsinnema@gmail.com>
2021-05-21 19:23:11 +0200

    updated settings

--- scidash/main/settings.py
+++ scidash/main/settings.py
@@ -286,9 +286,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'scidashasu@gmail.com'
-EMAIL_HOST_PASSWORD = 'Zh0UzMZDI7ic'
-EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'username'
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_USE_TLS = True
 
 POPULATE_USERS = True
 
